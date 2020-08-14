b4bd79114d312dd557f7647623e41328a1f6d01c
NAMANJAIN2606 <namanjainmail@gmail.com>
2020-08-14 13:18:22 +0530

    Updates

---
+++ precache-manifest.9ff4be66c5e955c439e44e233890b833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf806e015ab8d9ef79f2412c857f1779",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "d82bd9ba2a44a46c8fb5",
+    "url": "/todo-react/static/css/main.e8df13bf.chunk.css"
+  },
+  {
+    "revision": "e8eebf56b01e00826250",
+    "url": "/todo-react/static/js/2.65cbcdba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-react/static/js/2.65cbcdba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82bd9ba2a44a46c8fb5",
+    "url": "/todo-react/static/js/main.53f64330.chunk.js"
+  },
+  {
+    "revision": "e71ec557c49a4486f8ee",
+    "url": "/todo-react/static/js/runtime-main.f1c3f0f7.js"
+  }
+]);
